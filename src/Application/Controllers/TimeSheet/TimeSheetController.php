31d862d232ed3c9727471e48d3ee6a92c02b8218
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-04-06 16:20:46 +0200

    removed unused functions in controller

--- src/Application/Controllers/TimeSheet/TimeSheetController.php
+++ src/Application/Controllers/TimeSheet/TimeSheetController.php
@@ -17,142 +17,77 @@ use Psr\Log\LoggerInterface;
 class TimeSheetController extends Controller
 {
 
-    protected $postService;
-    protected $userService;
-    protected $outputEscapeService;
+    protected TimeSheetService $timeSheetService;
+    protected UserService $userService;
+    protected OutputEscapeService $outputEscapeService;
 
     public function __construct(
         LoggerInterface $logger,
-        TimeSheetService $postService,
+        TimeSheetService $timeSheetService,
         UserService $userService,
         OutputEscapeService $outputEscapeService
     ) {
         parent::__construct($logger);
-        $this->postService = $postService;
+        $this->timeSheetService = $timeSheetService;
         $this->userService = $userService;
         $this->outputEscapeService = $outputEscapeService;
     }
 
-    public function get(Request $request, Response $response, array $args): Response
+/*
+// Commented out because unused. If I'd use it, it would be done approx like this. Not tested though.
+
+public function get(Request $request, Response $response, array $args): Response
     {
-        $id = $args['id'];
-        $post = $this->postService->findPost($id);
+        $timeSheetId = $args['id'];
+        $timeSheet = $this->timeSheetService->findTimeSheet($timeSheetId);
 
-        // Get user information connected to post
-        $user = $this->userService->findUser($post['user_id']);
+        // Get user information connected to timeSheet
+        $user = $this->userService->findUser($timeSheet['user_id']);
 
-        // Add user name info to post
-        $postWithUser = $post;
-        $postWithUser['user_name'] = $user['name'];
+        // Add user name info to timeSheet
+        $timeSheetWithUser = $timeSheet;
+        $timeSheetWithUser['user_name'] = $user['name'];
 
-        $postWithUser = $this->outputEscapeService->escapeOneDimensionalArray($postWithUser);
-        return $this->respondWithJson($response, $postWithUser);
-    }
+        $timeSheetWithUser = $this->outputEscapeService->escapeOneDimensionalArray($timeSheetWithUser);
+        return $this->respondWithJson($response, $timeSheetWithUser);
+    }*/
 
     public function list(Request $request, Response $response, array $args)
     {
-        $postsWithUsers = $this->postService->findAllPosts();
+        $timeSheetsWithUsers = $this->timeSheetService->findAllTimeSheets();
 
         // output escaping only done here https://stackoverflow.com/a/20962774/9013718
-        $postsWithUsers = $this->outputEscapeService->escapeTwoDimensionalArray($postsWithUsers);
+        $timeSheetsWithUsers = $this->outputEscapeService->escapeTwoDimensionalArray($timeSheetsWithUsers);
 
-        return $this->respondWithJson($response, $postsWithUsers);
+        return $this->respondWithJson($response, $timeSheetsWithUsers);
 
     }
 
-    public function getOwnPosts(Request $request, Response $response, array $args): Response
-    {
-        // option 1 /posts?user=xxx and then $request->getQueryParams('user'); but that would mean that the user has to know its id
-        // option 2 /own-posts and get user id from token data body
+/*
+// Commented out because unused. If I'd use it, it would be done approx like this. Not tested though.
 
+public function getOwnTimeSheets(Request $request, Response $response, array $args): Response
+    {
         $loggedUserId = (int)$this->getUserIdFromToken($request);
 
-        $posts = $this->postService->findAllPostsFromUser($loggedUserId);
+        $timeSheets = $this->timeSheetService->findAllTimeSheetsFromUser($loggedUserId);
 
-        $posts = $this->outputEscapeService->escapeTwoDimensionalArray($posts);
+        $timeSheets = $this->outputEscapeService->escapeTwoDimensionalArray($timeSheets);
 
-        return $this->respondWithJson($response, $posts);
-    }
-
-    public function update(Request $request, Response $response, array $args): Response
-    {
-        $userId = (int)$this->getUserIdFromToken($request);
-
-        $id = (int)$args['id'];
-
-        $postFromDb = $this->postService->findPost($id);
-
-        // I write the role logic always for each function and not a general service "isAuthorised" function because it's too different every time
-        $userRole = $this->userService->getUserRole($userId);
-        // Check if it's admin or if it's its own post
-        if ($userRole === 'admin' || (int)$postFromDb['user_id'] === $userId) {
-
-            // todo check if parsedbody is empty everywhere
-            if (null !== $postData = $request->getParsedBody()) {
-                // todo maybe add mapping a layer between client body and application logic
-
-                $post = new TimeSheet(new ArrayReader($postData));
-                // Needed to tell repo what data to update
-                $post->setId($postFromDb['id']);
-
-                try {
-                    $updated = $this->postService->updatePost($post);
-                } catch (ValidationException $exception) {
-                    return $this->respondValidationError($exception->getValidationResult(), $response);
-                }
-
-                if ($updated) {
-                    return $this->respondWithJson($response, ['status' => 'success']);
-                }
-                $response = $this->respondWithJson($response,
-                    ['status' => 'warning', 'message' => 'The post was not updated']);
-                return $response->withAddedHeader('Warning', 'The post was not updated');
-            }
-            $response = $this->respondWithJson($response, ['status' => 'error', 'message' => 'Request body empty'],
-                400);
-            return $response->withAddedHeader('Warning', '');
-        }
-        $this->logger->notice('User ' . $userId . ' tried to update other post with id: ' . $id);
-        return $this->respondWithJson($response,
-            ['status' => 'error', 'message' => 'You have to be admin or post creator to update this post'], 403);
-    }
-
-    public function delete(Request $request, Response $response, array $args): Response
-    {
-        $userId = (int)$this->getUserIdFromToken($request);
-
-        $id = (int)$args['id'];
-
-        $post = $this->postService->findPost($id);
-
-        $userRole = $this->userService->getUserRole($userId);
-
-        // Check if it's admin or if it's its own post
-        if ($userRole === 'admin' || (int)$post['user_id'] === $userId) {
-
-            $deleted = $this->postService->deletePost($id);
-            if ($deleted) {
-                return $this->respondWithJson($response, ['status' => 'success']);
-            }
-            $response = $this->respondWithJson($response, ['status' => 'warning', 'message' => 'Post not deleted']);
-            return $response->withAddedHeader('Warning', 'The post got not deleted');
-        }
-        $this->logger->notice('User ' . $userId . ' tried to delete other post with id: ' . $id);
-        return $this->respondWithJson($response,
-            ['status' => 'error', 'message' => 'You have to be admin or post creator to update this post'], 403);
-    }
+        return $this->respondWithJson($response, $timeSheets);
+    }*/
 
     public function create(Request $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $userId = (int)$this->getUserIdFromToken($request);
 
-        if (null !== $postData = $request->getParsedBody()) {
+        if (null !== $timeSheetData = $request->getParsedBody()) {
 
-            $post = new TimeSheet(new ArrayReader($postData));
-            $post->setUserId($userId);
+            $timeSheet = new TimeSheet(new ArrayReader($timeSheetData));
+            $timeSheet->setUserId($userId);
 
             try {
-                $insertId = $this->postService->createPost($post);
+                $insertId = $this->timeSheetService->createTimeSheet($timeSheet);
             } catch (ValidationException $exception) {
                 return $this->respondValidationError($exception->getValidationResult(), $response);
             }
@@ -160,8 +95,8 @@ class TimeSheetController extends Controller
             if (null !== $insertId) {
                 return $this->respondWithJson($response, ['status' => 'success'], 201);
             }
-            $response = $this->respondWithJson($response, ['status' => 'warning', 'message' => 'Post not created']);
-            return $response->withAddedHeader('Warning', 'The post could not be created');
+            $response = $this->respondWithJson($response, ['status' => 'warning', 'message' => 'Time sheet not created']);
+            return $response->withAddedHeader('Warning', 'The time sheet could not be created');
         }
         return $this->respondWithJson($response, ['status' => 'error', 'message' => 'Request body empty']);
     }
