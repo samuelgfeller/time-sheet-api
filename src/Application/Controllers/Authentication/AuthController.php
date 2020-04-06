bc2249edf5e5c4aa5e951734f7cfd22a682d5f61
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-04-06 15:38:09 +0200

    Removed auto login after register and todo's in AuthController.php

--- src/Application/Controllers/Authentication/AuthController.php
+++ src/Application/Controllers/Authentication/AuthController.php
@@ -38,8 +38,7 @@ class AuthController extends Controller
 
         // Use Entity instead of DTO for simplicity https://github.com/samuelgfeller/slim-api-example/issues/2#issuecomment-597245455
         $user = new User(new ArrayReader($userData));
-        // Password gets hashed in service createUser($user) but is needed plain to build up login request body
-        $plainPass = $user->getPassword();
+
         try {
             $insertId = $this->userService->createUser($user);
         } catch (ValidationException $exception) {
@@ -50,32 +49,17 @@ class AuthController extends Controller
         if (null !== $insertId) {
             $this->logger->info('User "' . $userData['email'] . '" created');
 
-            // Add email and password like it is expected in the login function
-            $request = $request->withParsedBody(['email' => $userData['email'], 'password' => $plainPass]);
-            // Call login function to authenticate the user
-            // todo check if that is good practice or bad
-            $loginResponse = $this->login($request, $response);
-
-            $loginContent = json_decode($loginResponse->getBody(), true, 512, JSON_THROW_ON_ERROR);
-
-            // Clear response body after body content is saved
-            $response = new \Slim\Psr7\Response();
-
-            $responseContent = $loginContent;
-
-            // maybe there is already a message so it has to be transformed as array
-            $responseContent['message'] = [$loginContent['message']];
-            $responseContent['message'][] = 'User created and logged in';
-
-            return $this->respondWithJson($response, $responseContent);
+            return $this->respondWithJson(
+                $response,
+                ['status' => 'success', 'message' => 'User created successfully'],
+                201
+            );
         }
         return $this->respondWithJson($response, ['status' => 'error', 'message' => 'User could not be registered']);
     }
 
     public function login(Request $request, Response $response): Response
     {
-        // todo add check if already logged in
-
         $userData = $request->getParsedBody();
 
         $user = new User(new ArrayReader($userData));
@@ -92,7 +76,7 @@ class AuthController extends Controller
         } catch (ValidationException $exception) {
             return $this->respondValidationError($exception->getValidationResult(), $response);
         }
-        // todo prio1 catch invalidCredentialsException
+
         $this->logger->notice('Invalid login attempt from "' . $user->getEmail() . '"');
         return $this->respondWithJson($response, ['status' => 'error', 'message' => 'Invalid credentials'], 401);
     }
